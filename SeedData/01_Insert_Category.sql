c6f3c89ad434a2acea7636c3d6b3f1bb154a3e8c
youssefMaati <deabisyoussef@gmail.com>
2026-08-03 15:41:08 +0300

    add category seed data

---
+++ SeedData/01_Insert_Category.sql
@@ -0,0 +1,15 @@
+-- Insert Data To Cateory Table
+
+INSERT INTO Category(CategoryName)
+VALUES
+('Laptops'),
+('Smartphones'),
+('Accessories'),
+('Gaming'),
+('Networking'),
+('Storage'),
+('Monitors'),
+('Printers');
+
+SELECT * FROM Category
+ORDER BY CategoryID ASC
