ad6f41540afde0b22ab4b3d7302910dfddc88e6a
youssefMaati <deabisyoussef@gmail.com>
2026-08-03 16:11:54 +0300

    add suppliers seed data

---
+++ SeedData/02_Insert_suppliers.sql
@@ -0,0 +1,22 @@
+-- Insert Data to Supplier Table
+
+
+SELECT * FROM Supplier
+INSERT INTO Supplier
+(
+	CompanyName ,
+	PhoneNumber ,
+	Email ,
+	Address
+)
+VALUES
+('Dell Egypt',       '01010000001', 'contact@dell.com',      'Cairo'),
+('HP Egypt',         '01010000002', 'contact@hp.com',        'Giza'),
+('Lenovo Egypt',     '01010000003', 'contact@lenovo.com',    'Alexandria'),
+('Samsung Egypt',    '01010000004', 'contact@samsung.com',   'Cairo'),
+('Apple Distributor','01010000005', 'sales@apple-eg.com',    'Cairo'),
+('Logitech Egypt',   '01010000006', 'contact@logitech.com',  'Giza'),
+('TP-Link Egypt',    '01010000007', 'contact@tplink.com',    'Alexandria'),
+('Kingston Egypt',   '01010000008', 'contact@kingston.com',  'Cairo'),
+('Canon Egypt',      '01010000009', 'contact@canon.com',     'Giza'),
+('ASUS Egypt',       '01010000010', 'contact@asus.com',      'Cairo');
